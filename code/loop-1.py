7c915a9d49ab3efd705dc8b4caa75fd0d0829440
yjinsoo <91886289+yjinsoo@users.noreply.github.com>
2026-04-20 10:59:37 +0900

    Update loop-1.py

--- code/loop-1.py
+++ code/loop-1.py
@@ -19,5 +19,22 @@ my_cluster=[
 
 
 #출력
+# 리스트안에 딕셔너리가 pod에 딕셔너리로 대입
+##설명
+# f-string의 구조
+# f "문자열 {변수 or 계산식}"
+# 문자열 앞에 알파벳 f를 붙이면 파이썬은 문자열 안에 있는 중괄호 {}를 여기는 코드가 들어갈 자리구나 인식
 for pod in my_cluster:
   print(f"현대 검사중인 Pod: {pod['name']}")
+
+
+
+#if문 결합
+for pod in my_cluster:
+  if pod["status"] == "Error":
+    print(f"현재 pod상태이상 NAME: {pod['name']}, ID: {pod['id']}")
+  else:
+    print(f"정상 pod NAME: {pod['name]'}, ID: {pod['id]'}")
+
+
+

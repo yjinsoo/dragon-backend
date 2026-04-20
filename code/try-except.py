8f1dfac36e07fb3dbdd5b1b21090dc33bc43ecd0
yjinsoo <91886289+yjinsoo@users.noreply.github.com>
2026-04-20 14:29:25 +0900

    Update try-except.py

--- code/try-except.py
+++ code/try-except.py
@@ -1,1 +1,15 @@
+import time
 
+#에러 만들기
+#key값이 없는 경우
+error_date= { "name": "nginx-pod"}
+
+for pod in error_data:
+  if pod["status"] == "Running":
+    print('정상입니다'}
+
+
+
+print("--- 모든 검사가 끝났습니다. Pod를 유지합니다. ---")
+while True:
+    time.sleep(60) # 60초마다 한 번씩 쉬면서 무한 대기

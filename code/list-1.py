0c4f95880e4b699aec1db3d20eaed20d0d881586
yjinsoo <91886289+yjinsoo@users.noreply.github.com>
2026-04-20 10:01:06 +0900

    Update list-1.py

--- code/list-1.py
+++ code/list-1.py
@@ -2,8 +2,27 @@
 pods = ["web-server", "db-server", "cache-server"]
 
 #데이터 꺼내기
-print(pods[0])
-print(pods[-1]) 
+print(pods[0]) #결과 : web-server (첫번째 요소)
+print(pods[-1])  # 결과 : cache-server (마지막 요소)
 
 #데이터 추가하기
 pods.append("api-server") # 맨 뒤에 추가됨
+
+
+#딕셔너리 만들기 key, value형식
+pod_info = {
+  "name" : "nginx-pod",
+  "image" : "nginx:latest",
+  "cpu": "500m",
+  "memory": "128Mi"
+}
+
+#데이터 꺼내기
+print(pod_info["name"]) #결과 : nginx-pod
+print(pod_inf["memory"]) 
+
+#데이터 수정 및 추가
+pod_info["memory"] = "256Mi"
+
+#확인
+print(pod_inf["memory"])

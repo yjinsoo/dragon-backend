f28ad558f23236be62353f932c1a9e5544a50207
yjinsoo <91886289+yjinsoo@users.noreply.github.com>
2026-04-20 13:18:27 +0900

    Update loop-condition.py

--- code/loop-condition.py
+++ code/loop-condition.py
@@ -7,15 +7,18 @@ cluster_status = [
 ]
 
 #for문을 사용하여 전체 Pod검사
+print("for문을 사용하여 전체 Pod검사")
 for pod in cluster_status:
   print(f"POD name:{pod['name']}, Pod Status:{pod['status']}, POD cpu_usage: {pod['cpu_usage']}, Pod memory_usage: {pod['memory']}")
 
 #if문을 사용해 status가 "Running"인 Pod만 골라내기
+print("if문을 사용해 status가 "Running"인 Pod만 골라내기")
 for pod in cluster_status:
     if pod["status"] == "Running":
         print(f"{pod['name']} (상태: {pod['status']})")
 
 #cpu_usage가 80%이상인 POD 골라내기
+print("cpu_usage가 80%이상인 POD 골라내기")
 for pod in cluster_status:
     if pod["cpu_usage"] > 80:
         print(f"{pod['name']} (상태: {pod['status']}) - CPU: {pod['cpu_usage']}% [과부하]")

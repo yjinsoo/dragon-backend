cc872a19f572e997c214573b59f7e1b51b510f8a
yjinsoo <91886289+yjinsoo@users.noreply.github.com>
2026-04-20 10:24:24 +0900

    Update loop-1.py

--- code/loop-1.py
+++ code/loop-1.py
@@ -1,4 +1,23 @@
+##반복문
 #리스트 for문으로 출력하기
 pods = ["web-server", "db-server", "cache-server"]
 
-for 
+#딕셔너리 만들기 key, value형식
+pod_info = {
+  "name" : "nginx-pod",
+  "image" : "nginx:latest",
+  "cpu": "500m",
+  "memory": "128Mi"
+}
+
+# 리스트 안에 딕셔너리
+my_cluster=[
+  {"id":1, "name": "auth-pod", "status": "Running"},
+  {"id":2, "name": "payment-pod", "status": "Pending"},
+  {"id":3, "name": "search-pod", "status": "Error"}
+]
+
+
+#출력
+for pod in my_cluster:
+  print(f"현대 검사중인 Pod: {pod['name']}")

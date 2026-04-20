c2d840f29828b1bf63d4f920013b30f78ee6d13e
yjinsoo <91886289+yjinsoo@users.noreply.github.com>
2026-04-20 15:30:26 +0900

    Update example-1.py

--- code/example-1.py
+++ code/example-1.py
@@ -1,1 +1,19 @@
 import time
+
+inventory = [
+    {"item": "Milk", "price": 2500, "count": 10},
+    {"item": "Bread", "price": 1500, "count": 0},
+    {"item": "Coffee", "price": 3000, "count": 5},
+    {"item": "Snack", "price": 2000, "count": 2}
+]
+
+def check_inventory(item_list):
+  try:
+  if item_list["count"] == 0:
+    return "품절"
+  elif item_list["count"] <= 3:
+    return" 재입고 필요
+
+print("--- 모든 검사가 끝났습니다. Pod를 유지합니다. ---")
+while True:
+    time.sleep(60) # 60초마다 한 번씩 쉬면서 무한 대기

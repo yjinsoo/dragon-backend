ba72a50bdf96574ffa114acb97e6d76e7f2b5c9d
yjinsoo <91886289+yjinsoo@users.noreply.github.com>
2026-04-20 17:22:23 +0900

    Update example-2.py

--- code/example-2.py
+++ code/example-2.py
@@ -12,7 +12,7 @@ for stu in students:
     try:
         total=sum(stu["scores"])
         length=len(stu["scores"])
-        score_avg = sum / len
+        score_avg = total / length
         if score_avg >= 90:
             print(f"장학금 대상자 {stu['name']}, 평균점수 {score_avg}")
     except Exception as e:

a4c96da706c646b025197ca016e4cff1381fc95a
yjinsoo <91886289+yjinsoo@users.noreply.github.com>
2026-04-20 15:45:44 +0900

    Create example-2.py

---
+++ code/example-2.py
@@ -0,0 +1,15 @@
+import time
+
+students = [
+    {"name": "Kim", "scores": [80, 90, 85]},
+    {"name": "Lee", "scores": [100, 95, 90]},
+    {"name": "Park", "scores": [60, 70, 65]},
+    {"name": "Choi", "scores": [90, 90, 100]}
+]
+
+
+print(students[0].scores[
+
+print("======== end ==========")
+while true:
+  time.sleep(30)

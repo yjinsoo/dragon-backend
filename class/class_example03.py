d520831824086be91433e0e243ac16f1e8e245ac
yjinsoo <91886289+yjinsoo@users.noreply.github.com>
2026-04-21 14:40:04 +0900

    Create class_example03.py

---
+++ class/class_example03.py
@@ -0,0 +1,19 @@
+'''
+Pod 클래스를 만듭니다. (name, cpu를 속성으로 가짐)
+
+Pod 클래스 내부에 get_status() 메서드를 만듭니다.
+
+CPU가 80 이상이면 "⚠️ Danger", 아니면 "OK"를 반환합니다.
+
+반복문을 사용해 raw_data에 있는 딕셔너리들을 하나씩 Pod 객체로 만듭니다.
+
+각 객체의 get_status() 결과와 이름을 예쁘게 출력하세요.
+'''
+
+import time
+
+
+
+while True:
+  time.sleep(60)
+  

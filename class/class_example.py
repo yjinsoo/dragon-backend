7c6b2d9cc36b4a7f748002fca948254aee1f144f
yjinsoo <91886289+yjinsoo@users.noreply.github.com>
2026-04-21 11:10:22 +0900

    Update class_example.py

--- class/class_example.py
+++ class/class_example.py
@@ -4,7 +4,16 @@ class Pod:
   def __init__(self, name, cpu_usage):
     self.name = name
     self.cpu_usage = cpu_usage
-  def is_overload(
+    
+  def is_overload(self):
+    if self.cpu_usage >= 80 :
+      return True
+    else:
+      return False
+  
+object01 = Pod("DB-Pod",90)
+opject01.is_overload()
+
 
 print("==========END==========")
 while True:

63fb8e67112377395f43c58220f311d69069a788
yjinsoo <91886289+yjinsoo@users.noreply.github.com>
2026-04-21 10:16:05 +0900

    Update pip_requests.py

--- pip/pip_requests.py
+++ pip/pip_requests.py
@@ -4,14 +4,16 @@ requests 라이브러리
 '''
 
 import time,requests
+from pprint import pprint
 
 #1. 특정 주소에 GET 요청(데이터 달라고 하기)을 보냄
 response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
 
-print(f"원본 데이터: {response}")
-
 #2. 응답받은 데이터 파이썬 딕셔너리로 변경
 data = response.json()
+
+## key를 모를 때 데이터 확인
+pprint(data)
 
 #3. 우리가 배운 딕셔너리 사용법으로 데이터 출력
 print(f"가져온 글제목: {data['title']}")

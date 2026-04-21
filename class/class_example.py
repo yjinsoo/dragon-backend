9f157eb44dce2cdf3bc71d1ccc729401bf39398c
yjinsoo <91886289+yjinsoo@users.noreply.github.com>
2026-04-21 11:04:26 +0900

    Create class_example.py

---
+++ class/class_example.py
@@ -0,0 +1,11 @@
+import time
+
+class Pod:
+  def __init__(self, name, cpu_usage):
+    self.name = name
+    self.cpu_usage = cpu_usage
+  def is_overload(
+
+print("==========END==========")
+while True:
+  time.sleep(60)

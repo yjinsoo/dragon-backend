aced57b41fb1203d284267b82297fbaa912abf2a
yjinsoo <91886289+yjinsoo@users.noreply.github.com>
2026-04-23 10:06:09 +0900

    Update async_example05.py

--- asyncio/async_example05.py
+++ asyncio/async_example05.py
@@ -31,7 +31,7 @@ async def main():
   }
 
   async with httpx.AsyncClient() as client:
-    tasks = [check_db_connection(client, name, url) for name, url in services.itmes()]
+    tasks = [check_db_connection(client, name, url) for name, url in services.items()]
     await asyncio.gather(*tasks)
 
                          

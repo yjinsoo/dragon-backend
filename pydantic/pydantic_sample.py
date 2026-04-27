f65489dd8b942d099092a87f0f8f04a15f663752
yjinsoo <91886289+yjinsoo@users.noreply.github.com>
2026-04-27 13:55:58 +0900

    Update pydantic_sample.py

--- pydantic/pydantic_sample.py
+++ pydantic/pydantic_sample.py
@@ -5,7 +5,12 @@ class User(BaseModel):
   id: int # 무조건 숫자여야 함
   username: str #무조건 문자열이어  truths
 
-
+class ServerConfig(BaseModel):
+  name: str
+  #port는 최소 1024, 최대 65535이어야 함
+  port: int = Field(ge=1024, le=65535)
+  #vcpu는 1,2,4,8중 하나여야 함
+  vcpu: int = Field(description="CPU 코어 수")
 
 #테스트 데이터
 data = { "id": "123", "username": "jinsoo" }
@@ -14,6 +19,15 @@ user = User(**data)
 
 print(user.id)
 print(type(user.id))
+
+
+print(ServerConfig(name="web-server", port=8080, vcpu=2))
+
+try:
+  ServerConfig(name="db-server", port=80, vcpu=2)
+except Exception as e:
+  print(f"error check {e}")
+
 
 while True:
   time.sleep(60)

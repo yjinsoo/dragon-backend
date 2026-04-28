55a34d7c44200c143de85195f69a4f0efe285573
yjinsoo <91886289+yjinsoo@users.noreply.github.com>
2026-04-28 16:17:09 +0900

    Update models.py

--- project01/models.py
+++ project01/models.py
@@ -1,3 +1,10 @@
+'''
+DB를 객체처럼 다루게 해주는 + SQL 실행까지 해주는 라이브러리
+
+models.py는 그 위에 올릴 '데이터베이스 스키마 설계도' 로생각하면 되며,
+database.py에서 선언한 DB위에 하기 코드들로 table 생성 등을 진행
+'''
+
 from sqlalchemy import Column, Integer, String
 from database import Base
 

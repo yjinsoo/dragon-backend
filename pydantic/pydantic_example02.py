5aa816d840b85845404c7f1115f993fe02810e9f
yjinsoo <91886289+yjinsoo@users.noreply.github.com>
2026-04-28 10:31:30 +0900

    Update pydantic_example02.py

--- pydantic/pydantic_example02.py
+++ pydantic/pydantic_example02.py
@@ -3,17 +3,22 @@ from pydantic import BaseModel, Field
 from typing import Literal
 import asyncio
 
+user_list_db = {}
 app=FastAPI()
-
-
 
 class User(BaseModel):
   name: str
   age: int = Field(ge=0, le=100)
 
 
-@app.get("/create-user")
-async def create-user(user: User):
+@app.post("/create-user")
+async def create_user(user: User):
+  if user.name in user_list_db:
+    raise HTTPException(status_code=400, detail="이미 존재하는 USER")
+  user_list_db[user.name] = user
+
+
+@app.get("/check-user/{username}")
 
 
 

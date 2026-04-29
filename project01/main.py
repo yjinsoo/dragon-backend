e8e3c21efd55148b804cc98745b8639ad911df09
yjinsoo <91886289+yjinsoo@users.noreply.github.com>
2026-04-29 11:18:51 +0900

    Update main.py

--- project01/main.py
+++ project01/main.py
@@ -65,11 +65,13 @@ async def update_user(username: str, updatedata: UpdateUser, db: Session = Depen
     if not update_user:
         raise HTTPException(status_code = 404, detail="Update할 USER가 존재하지 않음")
     update_dic = updatedata.model_dump(exclude_unset = True)
+'''
     if update_dic["name"]:
         print(f"{update_dic['name']}")
     if update_dic["age"]:
         print(f"{update_dic['age']}")
-    return update_user
+'''
+    return update_dic
 
 
     

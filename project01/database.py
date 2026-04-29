f635afe0ed470066ccc5b3bf384d909b78417838
yjinsoo <91886289+yjinsoo@users.noreply.github.com>
2026-04-29 14:58:59 +0900

    Update database.py

--- project01/database.py
+++ project01/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./my_user_db.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, expire_on_commit=False)
 Base = declarative_base()
 
 # DB 세션을 얻는 함수

0b6d8cd440bb7785b72161ee37193c682ca0696c
yjinsoo <91886289+yjinsoo@users.noreply.github.com>
2026-05-06 17:42:30 +0900

    Update status.py

--- kubernetes/status.py
+++ kubernetes/status.py
@@ -1,1 +1,11 @@
+from fastapi import FastAPI, HTTPException, Depends, Header
+from sqlalchemy.orm import Session
+from pydantic import BaseModel, Field
+from typing import Literal, Optional
 
+
+app = FastAPI()
+
+@app.get("/pods")
+async def get_pods():
+  
